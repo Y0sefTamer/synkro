8ff7a36e61d273a7443432ef879523ce52938825
Y0sefTamer <programmeryosef@gmail.com>
2026-08-22 19:06:13 +0300

    feat(network): implement hyperswarm p2p connection and topic discovery

--- workers/main.js
+++ workers/main.js
@@ -1,2 +1,30 @@
-// the boilerplate worker is available at https://github.com/holepunchto/hello-pear-worker
-require('hello-pear-worker')
+const Pear = require('pear')
+const Hyperswarm = require('hyperswarm')
+const crypto = require('hypercore-crypto')
+const b4a = require('b4a')
+
+
+const swarm = new Hyperswarm()
+
+
+swarm.on('connection', (socket, info) => {
+ 
+  const peerId = b4a.toString(info.publicKey, 'hex').slice(0, 6)
+  
+
+  Pear.worker.message({ type: 'peer_connected', peerId })
+  
+ 
+  socket.write(`Hello buddy! The pipe is ready.`)
+
+ 
+  socket.on('data', (data) => {
+    Pear.worker.message({ type: 'data_received', data: data.toString() })
+  })
+})
+
+
+const topicName = 'meshdrive-secret-room-1'
+const topicBuffer = crypto.discoveryKey(b4a.from(topicName, 'utf8'))
+
+swarm.join(topicBuffer, { server: true, client: true })
